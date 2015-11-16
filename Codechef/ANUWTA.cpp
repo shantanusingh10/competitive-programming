5e0a94271e66635351d9cc5561e04919044602ab
Shantanu <shantanusingh994@gmail.com>
2015-11-16 07:55:08 +0530

    Create ANUWTA.cpp

---
+++ Codechef/ANUWTA.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+ 
+int main()
+{
+    ll T,i,N,M;
+    scanf("%lld",&T);
+    while(T--)
+    {
+         scanf("%lld",&N);
+         printf("%lld\n",(N*N+3*N)/2);
+    }
+return 0;
+}
