e0d573c371405c2125113f5270924530c8b0c4f3
Shantanu <shantanusingh994@gmail.com>
2015-11-16 07:58:55 +0530

    Create TEST.cpp

---
+++ Codechef/TEST.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdio>
+ 
+using namespace std;
+int main()
+{
+	while(1)
+	{
+		
+		int a;
+		scanf("%d",&a);
+		if(a!=42)
+		printf("%d\n",a);
+		else
+		break;
+	}
+	
+	
+	return 0;
+}
