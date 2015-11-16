ded9ee050361253ff2478c09302e66e13beb7f15
Shantanu <shantanusingh994@gmail.com>
2015-11-16 07:53:06 +0530

    Create GAME2048.cpp

---
+++ Codechef/GAME2048.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+using namespace std;
+ 
+int main()
+{
+    long long T,N;
+   scanf("%lld",&T);
+   while(T--)
+   {
+      scanf("%lld",&N);
+      printf("%lld\n",N-1);
+   }
+  return 0;
+}
